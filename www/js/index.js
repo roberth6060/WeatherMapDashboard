2c147b53ba8300a577319548072d17ec7c54daf8
Robert C. Haye <rch4538@rit.edu>
2025-03-20 17:20:01 +0100

    implement support for openstreetmap API

--- www/js/index.js
+++ www/js/index.js
@@ -1,24 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -32,14 +11,34 @@ navigator.geolocation.getCurrentPosition( onSuccess, onError, { timeout: 30000 }
  
 function onSuccess( position ) {
    if ( position.coords ) {
-      let lat = position.coords.latitude,
-        lng=position.coords.longitude,
+      let latitude = position.coords.latitude;
+      let longitude = position.coords.longitude;
+      let reverseGeocodeUrl = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
 
       //Google Maps
-        myLatlng = new google.maps.LatLng( lat, lng ),
+        myLatlng = new google.maps.LatLng( latitude, longitude),
         mapOptions = { zoom: 3, center: myLatlng },
-        map = new google.maps.Map( document.getElementById( 'map-canvas' ), mapOptions ),
+        map = new google.maps.Map( document.getElementById( "map-canvas" ), mapOptions ),
         marker = new google.maps.Marker( { position: myLatlng, map: map } );
+
+        //Add support for openstreetmap API to fetch location information 
+        fetch(reverseGeocodeUrl).then(reponse => reponse.json()).then(data=>{
+            let city = "";
+            let state = "";
+            let country = "";
+
+            // Get data about address
+            if (data.address) {
+                city = data.address.city || data.address.town || data.address.village || "";
+                state = data.address.state || data.address.region || "";
+                country = data.address.country || "";
+            }
+            
+            //Intialize location details: 
+            document.querySelector('.city').innerText = city || state || country;
+            document.querySelector('.coordinates').innerText = `Latitude: ${latitude.toFixed(2)}, Longitude: ${longitude.toFixed(2)}`;
+
+        });
    }
 }
 
